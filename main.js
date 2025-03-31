b599b9b80874fa34c1c4dc7745a249f287fd593b
joaquim feres <jucafpff@gmail.com>
2025-03-31 20:05:07 -0300

    setimo commit

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-function sendMail(){
+function sendMail() {
     let parms = {
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        feedback__text : document.getElementById("feedback__text").value,
-    }   
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        feedback__text: document.getElementById("feedback__text").value,
+    };
 
-    emailjs.send("service_cnqlqkw","template_lggaxu6",parms).then(alert("Email enviado!!"))
-
+    emailjs.send("service_cnqlqkw", "template_lggaxu6", parms)
+        .then(function(response) {
+            alert("Email enviado com sucesso!");
+        })
+        .catch(function(error) {
+            console.error("Erro ao enviar o email:", error);
+            alert("Ocorreu um erro ao enviar o email. Verifique os dados e tente novamente.");
+        });
 }
